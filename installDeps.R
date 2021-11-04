574e75372a9a07d080ffe87bd1c7221bf9d262eb
Julien Diot <juliendiot@ut-biomet.org>
2021-11-04 13:49:14 +0900

    remove unnessesary files

--- installDeps.R
+++
@@ -1,7 +0,0 @@
-# Author: Julien Diot juliendiot@ut-biomet.org
-# 2021 The University of Tokyo
-#
-# Description:
-# Install GWAS-engine dependencies
-
-install.packages(c("plumber", "git2r", "digest"))
